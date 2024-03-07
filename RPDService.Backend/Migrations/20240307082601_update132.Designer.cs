085ff1108449ede4618442cb302e44bd9fe19b7f
Danil <112797010+JordennJd@users.noreply.github.com>
2024-03-07 17:50:36 +0300

    update

--- RPDService.Backend/Migrations/20240307082601_update132.Designer.cs
+++ RPDService.Backend/Migrations/20240307082601_update132.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RPDSerice;
 
@@ -10,9 +11,11 @@ using RPDSerice;
 namespace RPDSerice.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240307082601_update132")]
+    partial class update132
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
